5d84cb1505a8bea80614160559e03cfad9d99610
Aleksandr Zinovyev <razor01rus@gmail.com>
2024-12-24 14:24:37 +0300

    Worked with models and migrations.

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# Из модуля models импортируем модель Category...
+from .models import Category
+
+# ...и регистрируем её в админке:
+admin.site.register(Category) 
