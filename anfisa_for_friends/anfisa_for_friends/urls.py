8d68363cdcc3a7f10f7e77f963108ebdf6e8f5a8
Aleksandr Zinovyev <razor01rus@gmail.com>
2024-12-25 14:05:53 +0300

    Added Debug toolbar for Django

--- anfisa_for_friends/anfisa_for_friends/urls.py
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.contrib import admin # type: ignore
+from django.urls import include, path # type: ignore
+from django.conf import settings # type: ignore
 
 urlpatterns = [
     path('', include('homepage.urls')),
@@ -7,3 +8,9 @@ urlpatterns = [
     path('ice_cream/', include('ice_cream.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# Если проект запущен в режиме разработки...
+if settings.DEBUG:
+    import debug_toolbar # type: ignore
+    # Добавить к списку urlpatterns список адресов из приложения debug_toolbar:
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
