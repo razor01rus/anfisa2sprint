4bbb9e7a975415e9d27df0ad762cec800ccfce1f
Aleksandr Zinovyev <razor01rus@gmail.com>
2024-12-29 09:02:20 +0300

    Added new functions.

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
+#from django.db.models import Q # type: ignore
+
+from ice_cream.models import IceCream
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    # Запрос:
+    ice_cream_list = IceCream.objects.values(
+        'id','title','description'
+        ).filter(
+        is_on_main=True, is_published=True
+        ).order_by('title')[1:4]
+    
+    # Полученный из БД QuerySet передаём в словарь контекста:
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
+    return render(request, template_name, context) 
--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render # type: ignore
+from django.shortcuts import get_object_or_404, render # type: ignore
 
+from ice_cream.models import IceCream
 
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
-    context = {}
+    ice_cream = get_object_or_404(IceCream.objects.values(
+        'title', 'description'
+        ).filter(is_published=True), 
+        pk=pk)
+    context = {
+        'ice_cream' : ice_cream,
+    }
     return render(request, template, context)
 
 
